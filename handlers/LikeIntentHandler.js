ac65ca4c7536619c7bbe25b45a43b980e9797268
imajoriri <imajo.srroni@gmail.com>
2018-08-09 13:17:20 +0900

    update LikeIntent

--- handlers/LikeIntentHandler.js
+++ handlers/LikeIntentHandler.js
@@ -4,7 +4,42 @@ exports.LikeIntentHandler = {
     return handlerInput.requestEnvelope.isMatch('LikeIntent');
   },
   handle: function(handlerInput){
-    var msg = "";
-    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
+    // TODO songTypeが1以外の時（いいねされたやつのとき）でももう一度いいねをしてbotに通知を送る
+    
+    // attributesからaudioInfoを取得
+    // serverにいいねされたことを通知
+    // --serverへのrequest json---
+    // {song: [obj], singer: [obj]}
+    // botにいいねした、歌と歌い手さんの情報を通知
+    // ユーザーに次に行くかどうかを聞く
+
+    var attributes = handlerInput.sessionAttributeManager.getSessionAttributes();
+
+    // attributesからaudioInfoを取得
+    var audioInfo = attributes.audioInfo;
+    
+    // serverにいいねされたことを通知
+    postLikeSongToServer(audioInfo.song, audioInfo.singer);
+
+    // botにいいねした歌い手と歌の情報を通知
+    postLikeSongToBot(audioInfo.song, audioInfo.singer);
+
+    // ユーザーに次の曲を聞くかどうかを聞く
+    var msg = "いいねしました。歌の詳細はLINEに送りました。次の曲を聞きますか？";
+    var reprompt = '次の曲を聞きますか？';
+    return handlerInput.responseBuilder
+      .speak(msg)
+      .reprompt(reprompt)
+      .getResponse();
   }
+}
+
+function postLikeSongToServer(song, singer){
+  console.log('post for server');
+  return this;
+}
+
+function postLikeSongToBot(song, singer){
+  console.log('post for bot');
+  return this;
 }
