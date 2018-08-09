2877d19c43d94e8b573d3ae699258d679c93526e
imajoriri <imajo.srroni@gmail.com>
2018-08-09 13:39:44 +0900

    update NextIntent

--- handlers/NextIntentHandler.js
+++ handlers/NextIntentHandler.js
@@ -4,7 +4,50 @@ exports.NextIntentHandler = {
     return handlerInput.requestEnvelope.isMatch('NextIntent');
   },
   handle: function(handlerInput){
-    var msg = "";
-    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
+    // attributesからsongtypeを取得
+    // userIdを取得
+    // songTypeとuserIdを乗せてgetAudioInfoでaudio情報を取得
+    // attributesを保存し直す
+    // msgを組み立てる
+
+    var attributes = handlerInput.sessionAttributeManager.getSessionAttributes();
+    const songType = attributes.songType;
+    const userId = handlerInput.requestEnvelope.session.user.userId;
+
+    // songtypeとuseridを乗せて/api/v1/cek/get_audioにpost
+    const audioInfo = getAudioInfo(songType, userId);
+
+    // attributes保存
+    const newAttributes = {
+      audioInfo: audioInfo,
+      songType: songType,
+    }
+    handlerInput.sessionAttributeManager.setSessionAttributes(newAttributes);
+
+    // audioのurl
+    // TODO song.file_urlを見るのではなくて、song.file_nameとドメインなどを組みあわせたものにするか
+    const audioURL = audioInfo.song.file_url;
+
+    if(songType === 1){
+      var lastMsg = `次の曲に進みますか？いいねしますか？`;
+    }else{
+      var lastMsg = `次の曲を聴きますか？`;
+    }
+
+    return handlerInput.responseBuilder
+      .audioPlay(audioURL)
+      .speak(lastMsg)
+      .reprompt(lastMsg)
+      .getResponse();
+  }
+}
+
+function getAudioInfo(songType, userId){
+  return {
+    song: {
+      file_url: "http://hoge",
+    },
+    singer: {
+    }
   }
 }
