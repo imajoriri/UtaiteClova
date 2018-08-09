c078c33a524f7ab6a6e19f93fc88ff302a927f4f
imajoriri <imajo.srroni@gmail.com>
2018-08-09 13:40:05 +0900

    update YesIntent

--- handlers/YesIntentHandler.js
+++ handlers/YesIntentHandler.js
@@ -1,10 +1,12 @@
+const { NextIntentHandler } = require('./NextIntentHandler.js');
 
 exports.YesIntentHandler = {
   canHandle: function(handlerInput){
-    return handlerInput.requestEnvelope.isMatch('YesIntent');
+    return handlerInput.requestEnvelope.isMatch('Clova.YesIntent');
   },
-  handle: function(handlerInput){
-    var msg = "";
-    return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
-  }
+  handle: NextIntentHandler.handle
+  //handle: function(handlerInput){
+  //  var msg = "";
+  //  return handlerInput.responseBuilder.speak(msg).reprompt(msg).getResponse();
+  //}
 }
