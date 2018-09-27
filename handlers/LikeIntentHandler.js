9250925eb60ea26ca9e68b5b30ca422d4d4bdce8
imajoriri <imajo.srroni@gmail.com>
2018-09-27 14:28:49 +0900

    友達追加していない人の処理

--- handlers/LikeIntentHandler.js
+++ handlers/LikeIntentHandler.js
@@ -1,3 +1,5 @@
+var rp = require('request-promise');
+
 var { postLikeSongToServer } = require('./../functions/postLikeSongToServer.js');
 var { postLikeSongToBot } = require('./../functions/postLikeSongToBot.js');
 const { soundEffects } = require('./../functions/soundEffects');
@@ -26,22 +28,56 @@ exports.LikeIntentHandler = {
     const alias = context.invokedFunctionArn.split(':').pop();
     const serverIP = process.env["serverIP" + "_" + alias];
 
-    // serverにいいねされたことを通知
-    //await postLikeSongToServer(audioInfo.song, userId);
-    postLikeSongToServer(audioInfo.song, userId, serverIP);
+    // // serverにいいねされたことを通知
+    // //await postLikeSongToServer(audioInfo.song, userId);
+    // postLikeSongToServer(audioInfo.song, userId, serverIP);
 
-    // botにいいねした歌い手と歌の情報を通知
-    await postLikeSongToBot(audioInfo.song, audioInfo.singer, userId, alias);
-    //postLikeSongToBot(audioInfo.song, audioInfo.singer, userId);
+    // // botにいいねした歌い手と歌の情報を通知
+    // // しかし、非同期にできないので踏み台Lambdaを作成している
+    // await postLikeSongToBot(audioInfo.song, audioInfo.singer, userId, alias);
+    // //postLikeSongToBot(audioInfo.song, audioInfo.singer, userId);
 
-    // ユーザーに次の曲を聞くかどうかを聞く
-    var msg = "いいねしました。歌の詳細はLINEに送りました。次の曲を聞きますか？";
-    var reprompt = '次の曲を聞きますか？';
-    return handlerInput.responseBuilder
-      .audioPlay(soundEffects.liked)
-      .speak(msg)
-      .reprompt(reprompt)
-      .getResponse();
+    // 友達追加されているかを確認。
+    var options = {
+      method: 'GET',
+      uri: 'https://api.line.me/v2/bot/profile/' + userId,
+      headers: {
+        "Authorization": "Bearer {" + process.env["channelAccessToken" + "_" + alias] + "}"
+      }
+    };
+
+    var response;
+    await rp(options)
+    // userがいる時
+      .then( async (data) => {
+        var msg = "いいねしました。歌の詳細はLINEに送りました。次の曲を聞きますか？";
+        var reprompt = '次の曲を聞きますか？';
+
+        await postLikeSongToBot(audioInfo.song, audioInfo.singer, userId, alias);
+        postLikeSongToServer(audioInfo.song, userId, serverIP);
+
+        response = handlerInput.responseBuilder
+          .audioPlay(soundEffects.liked)
+          .speak(msg)
+          .reprompt(reprompt)
+          .getResponse();
+      })
+    // userがいない時
+      .catch( err => {
+        console.log(err);
+        var msg = "いいねを送ろうとしましたが、友達追加されていないため送ることができませんでした。スキルストアより友達追加して、再度お試しください。";
+        
+        response = handlerInput.responseBuilder
+          .speak(msg)
+          .getResponse();
+      });
+
+    return response;
+    //return handlerInput.responseBuilder
+    //  .audioPlay(soundEffects.liked)
+    //  .speak(msg)
+    //  .reprompt(reprompt)
+    //  .getResponse();
   }
 }
 
