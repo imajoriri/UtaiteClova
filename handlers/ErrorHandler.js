3a499537faf80d5b57fe09d46b3bf018f318e5fb
imajoriri <imajo.srroni@gmail.com>
2018-10-02 15:53:59 +0900

    エラー時に通知

--- handlers/ErrorHandler.js
+++ handlers/ErrorHandler.js
@@ -1,9 +1,23 @@
+var rp = require('request-promise');
 
 exports.ErrorHandler = {
   canHandle: function(handlerInput){
     return true;
   },
-  handle: function(handlerInput){
+  handle: async function(handlerInput){
+    const url = "https://notify-api.line.me/api/notify";
+    const notifyKey = process.env["notifyKey"];
+    var options = {
+      method: 'POST',
+      uri: url,
+      form: {
+        message: "ボイスシンガーでエラーが発生しました。\n" + JSON.stringify(handlerInput)
+      },
+      headers: {
+        "Authorization": "Bearer " + notifyKey
+      }
+    };
+    await rp(options);
     var msg = "申し訳ございません。ボイスシンガーがうまく動きませんでした。また時間を開けてから試してください";
     return handlerInput.responseBuilder.speak(msg).getResponse();
   }
